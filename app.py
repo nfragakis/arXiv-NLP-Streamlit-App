fdcbc412155ad308c621098587eeca446a698161
Nick Fragakis <nafragak@ncsu.edu>
2020-11-18 16:03:30 -0600

    Updated embedding search for relevant docs

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from transformers import DistilBertTokenizer
-from networks.classification import DistillBertClass
 from data.categories import cat_map
 from sentence_transformers import SentenceTransformer
-from utils import *
+from utils import retrieve_model_classes, retrieve_arXiv_link, cosine
+import torch
 import numpy as np
 import pandas as pd
 import streamlit as st
-import json
 
 st.title('STEM Paper Discovery App')
 st.header('Relevant Fields:\n Biology, Computer Science, Economics, Math, Physics, and Statistics')
@@ -18,7 +17,7 @@ def get_data(*args):
     Decorated function to retrieve arxiv dataset
     from local directory and cache in pandas df
     """
-    return data_to_df(*args)
+    return pd.read_parquet('./data/arxiv_processed.pqt')
 
 @st.cache
 def get_embeddings():
@@ -26,24 +25,24 @@ def get_embeddings():
     Decorated function to retrieve pre-trained
     arXiv embeddings and cache in numpy array
     """
-    return retrieve_arXiv_embeddings()
+    return np.load('./data/embeddings.npy')
 
-def display_paper(n):
-    st.header(df.iloc[top_matches[n]]['title'].replace('\n ', ''))
+def display_paper(item):
+    st.header(item.title.replace('\n ', ''))
 
-    link = retrieve_arXiv_link(df.iloc[top_matches[n]]['id'])
+    link = retrieve_arXiv_link(item.id)
     st.markdown(f'**PDF Link:**\n[{link}]({link})')
 
-    st.text(df.iloc[top_matches[n]]['abstract'])
+    st.markdown(item.abstract)
 
 
-
-df = get_data(10000)
+df = get_data()
 embeddings = get_embeddings()
-tokenizer = DistilBertTokenizer.from_pretrained('./models/vocab_distilbert_arxiv.bin');
-classifier = torch.load('./models/pytorch_distilbert_arxiv.bin');
+tokenizer = DistilBertTokenizer.from_pretrained('./models/vocab_distilbert_arxiv.bin')
+classifier = torch.load('./models/pytorch_distilbert_arxiv.bin')
 
-sentence_transformer = SentenceTransformer('./models/bert-base-nli-mean-tokens')
+#sentence_transformer = SentenceTransformer('./models/bert-base-nli-mean-tokens')
+sentence_transformer = SentenceTransformer('./models/distilbert-base-nli-stsb-mean-tokens')
 model_classes = retrieve_model_classes()
 
 st.header('Enter a question or topic you would like to learn more about')
@@ -61,7 +60,7 @@ if user_query:
     mask = torch.tensor(query_tokens['attention_mask']).unsqueeze(0)
 
     pred = classifier(ids, mask)
-    top_val, top_idx = torch.topk(pred, 3, dim=1)
+    top_val, top_idx = torch.topk(pred[0], 3, dim=1)
     pred_categories = model_classes[top_idx].tolist()[0]
     pred_topics = [cat_map[cat] for cat in pred_categories]
 
@@ -76,10 +75,15 @@ if user_query:
     with top3:
         st.button(pred_topics[2])
 
-    # embeddings
+    # encode user query 
     query_embedding = sentence_transformer.encode(user_query)
+
+    # grab relevant embeddings based on category prediction
+    relevant_docs = df[df['categories'].isin(pred_categories)]
+    relevant_embeddings = embeddings[relevant_docs.index]
+
     sims = []
-    for ind, doc in embeddings.items():
+    for doc in relevant_embeddings:
         doc_sim = cosine(query_embedding, doc)
         sims.append(doc_sim)
 
@@ -87,6 +91,7 @@ if user_query:
     st.title('Most relevant papers')
 
     match = st.selectbox("Recommendation Number", [1, 2, 3], 0)
+    show_item = relevant_docs.iloc[top_matches[match - 1]]
 
-    display_paper(match-1)
+    display_paper(show_item)
 
